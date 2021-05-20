8d7beceae11f3d3e94b8d5f5d1b85644865569ca
drA666 <43187328+drA666@users.noreply.github.com>
2021-05-20 20:16:14 +0300

    lab3

---
+++ assets/books/images.js
@@ -0,0 +1,10 @@
+export default images = {
+    'Image_01.png': require('./Images/Image_01.png'),
+    'Image_02.png': require('./Images/Image_02.png'),
+    'Image_03.png': require('./Images/Image_03.png'),
+    'Image_05.png': require('./Images/Image_05.png'),
+    'Image_06.png': require('./Images/Image_06.png'),
+    'Image_07.png': require('./Images/Image_07.png'),
+    'Image_08.png': require('./Images/Image_08.png'),
+    'Image_10.png': require('./Images/Image_10.png'),
+};
